ca22a5fb8a3bbba921b09b8e457b261b744df961
kimyusub <rladbtjq18@jisan.com>
2024-08-05 17:37:30 +0900

    달력 테스트

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('pages/index');
+  res.render('pages/index', {
+    css: [],
+    js: []
+  });
 });
 
 router.get('/login', (req, res) => {
@@ -11,7 +14,19 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/calendar', (req, res) => {
-  res.render('pages/calendar');
+  res.render('pages/calendar', {
+    css: [
+      'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css',
+      // 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+      // 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css',
+    ],
+    js: [
+      { path: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js'},
+      { path: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales-all.min.js'},
+      // { path: '/js/pages/calender.js', type: 'module' }
+      { path: '/js/pages/calendar.js'}
+    ]
+  });
 });
 
 export default router;
