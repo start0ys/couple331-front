c78c2826a1f14bdd1fd64cf4574ffb72b9e10976
kimyusub <rladbtjq18@jisan.com>
2024-08-06 11:16:33 +0900

    logo 분리

--- public/js/common/main.js
+++ public/js/common/main.js
@@ -1,10 +1,22 @@
 import '@fontsource/lobster';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-document.getElementById('toggle-sidebar').addEventListener('click', () => {
-    const sidebar = document.getElementById('sidebar');
-    const main = document.getElementById('main');
-    
-    sidebar.classList.toggle('close-sidebar');
-    main.classList.toggle('close-main');
+document.addEventListener('DOMContentLoaded', () => {
+    bindEvent();
 });
+
+
+
+const bindEvent = () => {
+    // document.querySelector('#toggle-sidebar').addEventListener('click', () => {
+    //     const sidebar = document.querySelector('#sidebar');
+    //     const main = document.querySelector('#main');
+        
+    //     sidebar.classList.toggle('close-sidebar');
+    //     main.classList.toggle('close-main');
+    // });
+    document.querySelector('#logo-couple331').addEventListener('click', () => {
+        window.location.href='/';
+    });
+}
+
