60067769a3276bb06a35838d64ef7f7fc0403aa6
kimyusub <rladbtjq18@jisan.com>
2024-08-09 14:29:58 +0900

    달력 틀 잡는중

--- public/js/common/calendarHelper.js
+++ public/js/common/calendarHelper.js
@@ -4,7 +4,9 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 class CalendarHelper {
     constructor() {
-      this.calendars = {};
+        this.calendars = {};
+        this.schedules = {};
+        this.dayObj = { 0 : '일요일' ,1: '월요일' ,2: '화요일' ,3: '수요일' ,4: '목요일' ,5: '금요일' ,6: '토요일' };
     }
 
 
@@ -15,23 +17,24 @@ class CalendarHelper {
         const calendarEl = document.getElementById(calendarId);
 
         const defaultOption = {
-          plugins: [dayGridPlugin, interactionPlugin],
-          height: 'calc(100% - 25px)', // calendar 높이 설정
-          headerToolbar: {
-            left: '',
-            center: 'title',
-            right: ''
-          },
-          initialView: 'dayGridMonth',
-          expandRows: true, // 화면에 맞게 높이 재설정
-          slotMinTime: '00:00', // Day 캘린더에서 시작 시간
-          slotMaxTime: '24:00', // Day 캘린더에서 종료 시간
-          editable: true, // 수정 가능?
-          // selectable: true, // 달력 일자 드래그 설정가능
-          nowIndicator: true, // 현재 시간 마크
-          dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
-          longPressDelay: 100,
-          locale: 'ko', // 한국어 설정
+            plugins: [dayGridPlugin, interactionPlugin],
+        //   height: 'calc(100% - 25px)', // calendar 높이 설정
+            height: '85vh', // calendar 높이 설정
+            headerToolbar: {
+                left: '',
+                center: 'title',
+                right: ''
+            },
+            initialView: 'dayGridMonth',
+            expandRows: true, // 화면에 맞게 높이 재설정
+            slotMinTime: '00:00', // Day 캘린더에서 시작 시간
+            slotMaxTime: '24:00', // Day 캘린더에서 종료 시간
+            editable: true, // 수정 가능?
+            // selectable: true, // 달력 일자 드래그 설정가능
+            nowIndicator: true, // 현재 시간 마크
+            dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
+            longPressDelay: 100,
+            locale: 'ko', // 한국어 설정
       };
 
         const calendar = new Calendar(calendarEl, Object.assign({}, defaultOption, option));
@@ -46,7 +49,7 @@ class CalendarHelper {
      * @returns {Calendar}
      */
     getCalendar(calendarId) {
-      return this.calendars[calendarId] || null;
+        return this.calendars[calendarId] || null;
     }
 
     /**
@@ -54,23 +57,71 @@ class CalendarHelper {
      * @param {"next" | "prev" | "today"} target 
      */
     changeMonth(calendarId, target) {
-      const calendar = this.getCalendar(calendarId);
+        const calendar = this.getCalendar(calendarId);
         
-      if (!calendar || !['next', 'prev', 'today'].includes(target)) {
-          return;
-      }
+        if (!calendar || !['next', 'prev', 'today'].includes(target)) {
+            return;
+        }
 
-      calendar[target]();
+        calendar[target]();
     }
 
-    setSchedule(calendarId, schedule) {
-      const calendar = this.getCalendar(calendarId);
-        
-      if (!calendar || !schedule || typeof schedule !== 'object') {
-          return;
-      }
+    getDateStr(date, pattern = 'yyyy-MM-dd') {
+        if(!date instanceof Date) return '';
 
-      calendar.addEvent(schedule);
+        const getTimeNumber = time => time < 10 ? '0' + time : time;
+        const year = date.getFullYear();
+        const month = getTimeNumber(date.getMonth() + 1);
+        const day = getTimeNumber(date.getDate());
+
+        let str = '';
+
+        switch(pattern) {
+            case 'yyyy-MM-dd':
+                str = `${year}-${month}-${day}`;
+                break
+            case 'yyyy년 MM월 dd일 E요일': 
+                str = `${year}년 ${month}월 ${day}일 ${this.dayObj[date.getDay()]}`;
+                break;
+        }
+
+        return str;
+    }
+
+    
+
+    setSchedule(calendarId, schedule) {
+        const calendar = this.getCalendar(calendarId);
+            
+        if (!calendar || !schedule || typeof schedule !== 'object') {
+            return;
+        }
+        
+        calendar.addEvent(schedule);
+
+        const startDate = schedule.start;
+        const endDate = schedule.end;
+        const title = schedule.title;
+
+        let currentDate = new Date(startDate);
+
+        if(startDate === endDate) {
+            const details = this.schedules[startDate] || [];
+            details.push(title);
+            this.schedules[startDate] = details;
+        } else {
+            while (currentDate < new Date(endDate)) {
+                const date = this.getDateStr(currentDate, 'yyyy-MM-dd');
+                const details = this.schedules[date] || [];
+                details.push(title);
+                this.schedules[date] = details;
+                currentDate.setDate(currentDate.getDate() + 1);
+            }
+        }
+    }
+
+    getDetailSchedules(day) {
+        return this.schedules[day] || [];
     }
 }
 
--- public/js/pages/schedule.js
+++ public/js/pages/schedule.js
@@ -6,22 +6,12 @@ const CALENDAR_ID = 'calendar';
 let picker = null;
 
 document.addEventListener('DOMContentLoaded', function() {
-	CalendarHelper.init(CALENDAR_ID, {dateClick: dateClick});
-  	bindEvent();
+	initCalendar();
+	bindEvent();
   	setDatePicker();
-  	dateClick({
-		date: new Date(),
-		dayEl: document.querySelector('.fc-day-today')
-	});
 });
 
 const bindEvent = () => {
-  	document.querySelectorAll('[change-type]').forEach(btn => {
-    	btn.addEventListener('click', () => {
-        	CalendarHelper.changeMonth(CALENDAR_ID, btn.getAttribute('change-type'));      
-    	})
-  	})
-
 	document.querySelectorAll('[target-date]').forEach(dateIcon => {
     	dateIcon.addEventListener('click', () => {
         	const targetId = dateIcon.getAttribute('target-date');
@@ -29,8 +19,8 @@ const bindEvent = () => {
     	})
   	})
 
-	document.getElementById('addSchedule').addEventListener('click', () => {
- 		const date = new Date(document.getElementById('todoDay').value);
+	document.querySelector('.schedule-modal-opener').addEventListener('click', () => {
+ 		const date = new Date(document.getElementById('targetDay').value);
     	picker.setStartDate(date);
     	picker.setEndDate(date);
   	})
@@ -43,7 +33,7 @@ const bindEvent = () => {
 		if(startDateStr !== endDateStr) {
 			const endDate = new Date(endDateStr);
 			const tomorrow = new Date(endDate.setDate(endDate.getDate() + 1)); // fullcalender에 기간 표시될 때 마지막날에서 하루 더 해야함
-			endDateStr = getDateStr(tomorrow).day;
+			endDateStr = CalendarHelper.getDateStr(tomorrow, 'yyyy-MM-dd');
 		}
 
 		CalendarHelper.setSchedule(CALENDAR_ID, {
@@ -59,29 +49,14 @@ const bindEvent = () => {
 	})
 }
 
-const getDateStr = (date) =>{
-	const dayObj = { 0 : '일요일' ,1: '월요일' ,2: '화요일' ,3: '수요일' ,4: '목요일' ,5: '금요일' ,6: '토요일' };
-	const getTimeNumber = time => time < 10 ? '0' + time : time;
-	const year = date.getFullYear();
-	const month = getTimeNumber(date.getMonth() + 1);
-	const day = getTimeNumber(date.getDate());
-	return {
-		dayStr: `${year}년 ${month}월 ${day}일 ${dayObj[date.getDay()]}`,
-		day: `${year}-${month}-${day}`
-	}
-}
-
 const scheduleDetail = (date) => {
 	if(!date) return;
 
-	const todoDayStrEl = document.getElementById('todoDayStr');
-	const todoDayEl = document.getElementById('todoDay');
+	const targetDayKoEl = document.getElementById('targetDayKo');
+	const targetDayEl = document.getElementById('targetDay');
 
-
-	const dateObj = getDateStr(date)
-
-	todoDayStrEl.innerText = dateObj.dayStr;
-	todoDayEl.value = dateObj.day;
+	targetDayKoEl.innerText = CalendarHelper.getDateStr(date, 'yyyy년 MM월 dd일 E요일');
+	targetDayEl.value = CalendarHelper.getDateStr(date, 'yyyy-MM-dd');
 }
 
 const dateClick = (info) => {
@@ -92,10 +67,16 @@ const dateClick = (info) => {
 	document.querySelectorAll(`.${className}`).forEach(i => i.classList.remove(className));
 
 	info.dayEl.classList.add(className);
+
+	const day = CalendarHelper.getDateStr(info.date);
+	const detailEl = document.getElementById('detailList');
+	const TITLE_TEMPLATE = (title) => '<div>'+title+'</div>';
+	detailEl.innerHTML = '';
+	CalendarHelper.getDetailSchedules(day).forEach(title => detailEl.insertAdjacentHTML("beforeend", TITLE_TEMPLATE(title)))
 }
 
 const setDatePicker = () => {
-	const date = document.getElementById('todoDay').value;
+	const date = document.getElementById('targetDay').value;
 	picker = DatePicker.createRangePicker({
 			startpicker: {
 				date: new Date(date),
@@ -111,5 +92,41 @@ const setDatePicker = () => {
 			format: 'YYYY-MM-dd'
 		});
 
+}
+
+const initCalendar = () => {
+	const calendarOption = {
+		dateClick: dateClick,
+		headerToolbar: {
+            left: 'prev',
+            center: 'title',
+            right: 'customToday next'
+        },
+		customButtons:{
+			prev:{ text:'<', click: () => CalendarHelper.changeMonth(CALENDAR_ID, 'prev') },
+			next:{ text:'>', click: () => CalendarHelper.changeMonth(CALENDAR_ID, 'next') },
+			customToday:{ text:'오늘', click: () => {
+				CalendarHelper.changeMonth(CALENDAR_ID, 'today');
+				dateClick({
+					date: new Date(),
+					dayEl: document.querySelector('.fc-day-today')
+				}); }
+			}
+		},
+		dayCellContent: info => {
+			const number = document.createElement('a');
+			number.classList.add('view-mode-day-number');
+			number.innerHTML = info.dayNumberText.replace('일', '');
+			return {
+				html : number.outerHTML
+			};
+		  }
+	}
+
+	CalendarHelper.init(CALENDAR_ID, calendarOption);
+  	dateClick({
+		date: new Date(),
+		dayEl: document.querySelector('.fc-day-today')
+	});
 }
 
