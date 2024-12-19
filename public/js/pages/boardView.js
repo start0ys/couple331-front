8bf4e48720628950629b0778f35aef4025443133
시작 <rladbtjq18@naver.com>
2024-12-20 00:40:17 +0900

    댓글 무한 스크롤 구현

--- public/js/pages/boardView.js
+++ public/js/pages/boardView.js
@@ -18,14 +18,30 @@ const CATEGORY_OBJ = Object.freeze({
 
 let _size = 10;
 let _page = 0
+let _isLoading = false;
+let _saveCount = 0;
 
 
 document.addEventListener('DOMContentLoaded', () => {
     CommentHelper.init('commentInputArea', saveComment);
-    setData();
+    initData();
+    window.addEventListener('scroll', handleScroll);
 });
 
-const setData = () => {
+const handleScroll = async () => {
+    if (_isLoading) return;
+  
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  
+    // 스크롤이 하단에 도달했는지 확인
+    if (scrollTop + clientHeight >= scrollHeight - 30) {
+        _isLoading = true;
+        _isLoading = await setCommentData();
+        _saveCount = 0;
+    }
+  }
+
+const initData = () => {
     
     handleApiResponse(
         () => request('get', `/api/board/${_boardId}?page=${_page}&size=${_size}`, null),
@@ -41,15 +57,41 @@ const setData = () => {
             document.getElementById('createDateTime').textContent = getDateStr(new Date(createDateTime), 'yyyy-MM-dd hh:mm');
             initEditor(content);
 
-            _page = commentInfo.number || _page;
+            // _page = commentInfo.number || _page;
+            _page++;
             const comments = commentInfo.content || [];
             for(const comment of comments) {
                 const parentId = comment.parentId || '';
                 CommentHelper.setCommnet(COMMENT_AREA_ID + parentId, comment);
             }
+            if(comments.length === 0) window.removeEventListener('scroll', handleScroll);
         },
         true,
         ''
+    );
+}
+
+const setCommentData = async () => {
+    
+    return handleApiResponse(
+        () => request('get', `/api/board/${_boardId}/comment?page=${_page}&size=${_size}`, null),
+        (res) => {
+            const data = res?.data || {};
+
+            // _page = commentInfo.number || _page;
+            _page++;
+            const comments = data.content || [];
+            const size = comments.length;
+            for(let i = _saveCount; i < size; i++) {
+                const comment = comments[i];
+                const parentId = comment.parentId || '';
+                CommentHelper.setCommnet(COMMENT_AREA_ID + parentId, comment);
+            }
+            if(size === 0) window.removeEventListener('scroll', handleScroll);
+            return false;
+        },
+        true,
+        false
     );
 }
 
@@ -69,6 +111,7 @@ const saveComment = (parentId = '', parentName = '') => {
     handleApiResponse(
         () => request('post', `/api/board/${_boardId}/comment/register`, data),
         (res) => {
+            _saveCount++;
             const comment = res?.data || data;
             comment.author = _name;
             comment.parentAuthor =  parentName;
